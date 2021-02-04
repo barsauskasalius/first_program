ccea415a85bacf9cce2307caff5cf3d5dd4fe931
Alius <barsauskasalius@gmail.com>
2021-02-04 16:31:20 +0100

    Pirmos 2 uzduotys

---
+++ body_parts.py
@@ -0,0 +1,10 @@
+def fix_the_meerkat(arr):
+    def find_difference(a, b):
+        a = ([0] * [1] * [2])
+        b = ([0] * [1] * [2])
+        if a > b:
+            return a - b
+        else:
+            return b - a
+    print int(fix_the_meerkat(arr))
+        # Your code here!
