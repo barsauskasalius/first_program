3190b9b1f8acf3abd2b87e18f5d7b8885d999fb7
Alius <barsauskasalius@gmail.com>
2021-02-07 16:33:02 +0100

    Pirmos  uzduotys

---
+++ Bmi_calculator/Bmi_calculator_in_meters_and_cm.py
@@ -0,0 +1,25 @@
+print("Enter your weight in kg:")
+weight = float(input())
+print("Enter your height in meters or centimeters:")
+height = input()
+def bmi_calculator():
+    if height[1] != ".":
+        kmi: float = round((weight / float(height) ** 2) * 10000)
+        return kmi
+    else:
+        kmi: float = round(weight / float(height) ** 2)
+        return kmi
+def compare():
+    if kmi < 18.5:
+        print("Underweight")
+    elif 18.5 <= kmi < 25:
+        print("Normal")
+    elif 25 <= kmi < 30:
+        print("Overweight")
+    else:
+        print("Obesity")
+bmi_calculator()
+kmi = bmi_calculator()
+print("Your kmi is:")
+print(kmi)
+compare()
