994adff174ce62157bfde50930719bb39a54aa6e
Alius <barsauskasalius@gmail.com>
2021-02-07 11:36:40 +0100

    Pirmos  uzduotys

---
+++ Codewars/8kyu/Quarter of the year.py
@@ -0,0 +1,10 @@
+def quarter_of(month):
+    if month <= 3 and month > 0:
+        return 1
+    elif month > 3 and month <= 6:
+        return 2
+    elif month > 6 and month <= 9:
+        return 3
+    elif month > 10 and month <= 12:
+        return 4
+## https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/python
