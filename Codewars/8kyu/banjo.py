bf7def1d2c81c846b623e58212c6aaefabdc2fc0
Alius <barsauskasalius@gmail.com>
2021-02-08 16:21:25 +0100

    Pirmos  uzduotys

---
+++ Codewars/8kyu/banjo.py
@@ -0,0 +1,6 @@
+def are_you_playing_banjo(name):
+    if name[0] in ['R', 'r']:
+        return name + " plays banjo"
+    else:
+        return name + " does not play banjo"
+    # https://www.codewars.com/kata/53af2b8861023f1d88000832/train/pythonbanjo"
