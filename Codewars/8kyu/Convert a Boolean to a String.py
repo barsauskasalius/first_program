08cad73eabccd73c7d42bb7f7b5dca8bc593e14f
Alius <barsauskasalius@gmail.com>
2021-02-08 16:09:24 +0100

    Pirmos  uzduotys

---
+++ Codewars/8kyu/Convert a Boolean to a String.py
@@ -0,0 +1,8 @@
+def boolean_to_string(b):
+    if b == True:
+        return "True"
+    elif b == False:
+        return "False"
+
+
+#https: // www.codewars.com / kata / 551b4501ac0447318f0009cd / train / python
