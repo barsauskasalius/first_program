a7bf5996c634a432ca87ccb0780079d481dd575a
Alius <barsauskasalius@gmail.com>
2021-02-09 13:36:15 +0100

    Pirmos  uzduotys

---
+++ Codewars/8kyu/Invert values.py
@@ -0,0 +1,5 @@
+def invert(lst):
+    negativelist = [-x for x in lst]
+    return negativelist
+print (invert([-1, 2, 3, 4, 5]))
+#https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/train/python
