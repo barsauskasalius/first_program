f42be1ff0d7fc420377a9acac8832599a585cca6
Alius <barsauskasalius@gmail.com>
2021-02-09 16:08:52 +0100

    Pirmos  uzduotys

--- Codewars/8kyu/Sum of positive.py
+++ Codewars/8kyu/Sum of positive.py
@@ -3,3 +3,9 @@ def positive_sum(arr):
     return sum(visas)
 print(positive_sum([1,-4,7,12]))
 #https://www.codewars.com/kata/5715eaedb436cf5606000381/train/python
+def postive_sum(arr):
+    sum = 0
+    for e in arr:
+        if e > 0:
+            sum = sum + e
+    return sum
