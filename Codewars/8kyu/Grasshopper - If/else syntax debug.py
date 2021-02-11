219bc4a85af51886455c47f504b786207102eeb3
Alius <barsauskasalius@gmail.com>
2021-02-11 12:08:39 +0100

    Fix bug in if statement

---
+++ Codewars/8kyu/Grasshopper - If/else syntax debug.py
@@ -0,0 +1,6 @@
+def check_alive(health):
+    if health <= 0:
+        return False
+    elif health > 0:
+        return True
+    #https://www.codewars.com/kata/57089707fe2d01529f00024a/train/python
