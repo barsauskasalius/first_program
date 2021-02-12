4f8a942de50e5272f55bf23c45052bdd6e440f28
Alius <barsauskasalius@gmail.com>
2021-02-12 09:52:12 +0100

    Find first digit of number

--- Codewars/8kyu/validate code with simple regex.py
+++ Codewars/8kyu/validate code with simple regex.py
@@ -6,7 +6,7 @@ def validate_code(code):
         return False
 
 print(validate_code(11))
-#https://www.codewars.com/kata/56a25ba95df27b7743000016/train/python
+##https://www.codewars.com/kata/56a25ba95df27b7743000016/train/python
 def validate_code(code):
     return str(code).startswith(('1', '2', '3'))
 def validate_code(code):
