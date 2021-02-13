3d585958885381b18b44a8df59c84cc535aeb1d3
Alius <barsauskasalius@gmail.com>
2021-02-13 08:18:33 +0100

    Convert numbers to list

---
+++ Codewars/8kyu/What is between.py
@@ -0,0 +1,8 @@
+def between(a,b):
+    arr = []
+    for i in range(a, b + 1):
+        arr.append(i)
+    return arr
+#https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/python
+def between(a,b):
+    return list(range(a,b+1))
